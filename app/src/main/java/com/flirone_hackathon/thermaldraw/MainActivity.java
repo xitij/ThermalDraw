8b697c1cec3b3f170f78ff14879bafb9e5220b9d
Tarik Ourchane <tarik.ourchane@starmakerinteractive.com>
2015-06-27 14:22:19 -0700

    Created a simple MainActivity with some buttons.

--- app/src/main/java/com/flirone_hackathon/thermaldraw/MainActivity.java
+++ app/src/main/java/com/flirone_hackathon/thermaldraw/MainActivity.java
@@ -2,36 +2,64 @@ package com.flirone_hackathon.thermaldraw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView mShapeTextView;
+    private Button mStartButton;
+    private Button mCaptureButton;
+    private int mClickCount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "onCreate()");
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onResume() {
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "onResume()");
         }
+        super.onResume();
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void initViews() {
+        mShapeTextView = (TextView) findViewById(R.id.shape_text);
+        mStartButton = (Button) findViewById(R.id.start_button);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleStartClick(v);
+                mClickCount++;
+            }
+        });
+        mCaptureButton = (Button) findViewById(R.id.capture_button);
+        mCaptureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Capture button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void handleStartClick(View v) {
+        // Choose a shape and populate the Shape Text View
+        String [] shapesArray = getResources().getStringArray(R.array.shapes_array);
+        int idx = mClickCount % shapesArray.length;
+        String shapeText = shapesArray[idx];
+        mShapeTextView.setText(shapeText);
     }
 }
